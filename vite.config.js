f845dad562c1a39903a4a1f3f98a1c8a1e0948f6
Arlind Sinojmeri <sinojmeri.arlind@gmail.com>
2024-08-24 12:15:00 +0200

    bug fix 1.1

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '192.168.1.44+3-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, '192.168.1.44+3.pem')),
-    },
-    host: '192.168.1.44',
-    port: 5173
-  },
 });
