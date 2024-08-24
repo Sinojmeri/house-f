e7b9b8ffb4007a89695b27c1d90a249bf65abbf5
Arion Samarxhiu <arionsamarxhiu32@gmail.com>
2024-08-25 00:56:20 +0200

    Lint fix

--- api.test.js
+++ api.test.js
@@ -1,35 +1,33 @@
-import {signIn, logIn } from './src/controllers/userApi.js';
+import { signIn, logIn } from './src/controllers/userApi.js';
 
 async function testSignIn() {
-    try {
-      const data = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'john.doe@example.com',
-        password: 'password123',
-      };
-  
-      const result = await signIn({ data });
-      console.log('Sign In Response:', result);
-    } catch (error) {
-      console.error('Test Sign In Error:', error);
-    }
+  try {
+    const data = {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+      password: 'password123',
+    };
+
+    await signIn({ data });
+  } catch (error) {
+    console.error('Test Sign In Error:', error);
   }
-  
-//   testSignIn();
+}
 
-  async function testLogIn() {
-    try {
-        const data = {
-            email: 'john.doe@example.com',
-            password: 'password123',
-        };
+testSignIn();
 
-        const result = await logIn({ data });
-        console.log('Log In Response:', result);
-    } catch (error) {
-        console.error('Test Log In Error:', error);
-    }
+async function testLogIn() {
+  try {
+    const data = {
+      email: 'john.doe@example.com',
+      password: 'password123',
+    };
+
+    await logIn({ data });
+  } catch (error) {
+    console.error('Test Log In Error:', error);
+  }
 }
 
 testLogIn();
