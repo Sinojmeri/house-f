805604ca57c8efd71cee67ef95ff84b6e2a79fd9
Sinojmeri <sinojmeri.arlind@gmail.com>
2024-08-24 22:10:15 +0200

    Map fix v1.1

--- src/components/MapComp.jsx
+++ src/components/MapComp.jsx
@@ -1,13 +1,9 @@
+import PropTypes from 'prop-types';
+
 const API_Key = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
 export const mapId = 'af816b56ad25fdab';
-import PropTypes from 'prop-types';
-import {
-  APIProvider,
-  Map,
-  Pin,
-  AdvancedMarker,
-  InfoWindow,
-} from '@vis.gl/react-google-maps';
+import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
+
 import { useEffect, useState } from 'react';
 
 const options = {
@@ -21,36 +17,23 @@ function error(err) {
   console.error(`ERROR(${err.code}): ${err.message}`);
 }
 
-function PoiMarker({ pois, setActiveMarker }) {
-  return (
-    <>
-      {pois.map((poi) => (
-        <AdvancedMarker
-          key={poi.key}
-          position={poi.location}
-          onClick={() => setActiveMarker(poi.key)}
-        >
-          <Pin
-            background={'#ff0000'}
-            glyphColor={'#000'}
-            borderColor={'#000'}
-          />
-        </AdvancedMarker>
-      ))}
-    </>
-  );
-}
-PoiMarker.propTypes = {
-  pois: PropTypes.array,
-  setActiveMarker: PropTypes.func,
-};
-export default function MapComp() {
+export default function MapComp({ houseCoords, setHouseCoords }) {
   const [myLocation, setMyLocation] = useState(null);
-  const [activeMarker, setActiveMarker] = useState(null);
+  const [center, setCenter] = useState(null);
+  const [zoom, setZoom] = useState(15);
+
   const locations = [
-    { key: 'myLocation', location: myLocation },
     { key: 'One Lapraka', location: { lat: 41.34042, lng: 19.788418 } },
     { key: 'One DTA', location: { lat: 41.337806, lng: 19.835278 } },
+    { key: "Arlindi's Home", location: { lat: 41.326103, lng: 19.827426 } },
+    { key: 'Hoxha Tasim', location: { lat: 41.333375, lng: 19.832833 } },
+    { key: 'Gazeta Koha Jone', location: { lat: 41.321583, lng: 19.813164 } },
+    { key: 'Aba Center', location: { lat: 41.320301, lng: 19.823113 } },
+    { key: 'Kullat', location: { lat: 41.322576, lng: 19.81957 } },
+    { key: 'Rr.Kavajes', location: { lat: 41.327795, lng: 19.811548 } },
+    { key: 'Vizion Plus', location: { lat: 41.337028, lng: 19.803694 } },
+    { key: '21 Dhjetori', location: { lat: 41.32609, lng: 19.802995 } },
+    { key: 'Test location', location: { lat: 41.325634, lng: 19.830913 } },
   ];
 
   useEffect(() => {
@@ -62,39 +45,73 @@ export default function MapComp() {
     return () => navigator.geolocation.clearWatch(id);
   }, []);
 
+  useEffect(() => {
+    if (houseCoords) {
+      setCenter(houseCoords);
+      setZoom(18);
+    }
+  }, [houseCoords]);
+  MapComp.propTypes = {
+    houseCoords: PropTypes.object,
+    setHouseCoords: PropTypes.func,
+  };
   return (
     <div className="relative w-full md:h-[700px] h-[400px] my-3">
       {myLocation ? (
         <APIProvider apiKey={API_Key}>
-          <Map
-            defaultZoom={15}
-            defaultCenter={myLocation}
-            mapId={mapId}
-            mapTypeId="roadmap"
-            className="w-full h-full"
-          >
-            <PoiMarker pois={locations} setActiveMarker={setActiveMarker} />
-
-            {locations.map((location) =>
-              activeMarker === location.key ? (
-                <InfoWindow
-                  key={location.key}
-                  position={location.location}
-                  onCloseClick={() => setActiveMarker(null)}
-                  className=""
-                >
-                  {location.key}
-                </InfoWindow>
-              ) : null,
-            )}
-            <AdvancedMarker position={myLocation}>
-              <Pin
-                background={'#0000ff'}
-                glyphColor={'#fff'}
-                borderColor={'#000'}
-              />
-            </AdvancedMarker>
-          </Map>
+          {houseCoords ? (
+            <Map
+              zoom={zoom}
+              center={center || myLocation}
+              onCenterChanged={() => {
+                setHouseCoords(null);
+              }}
+              defaultZoom={15}
+              mapId={mapId}
+              mapTypeId="roadmap"
+              streetViewControl={false}
+              mapTypeControl={false}
+              gestureHandling={'greedy'}
+              zoomControl={false}
+              className="w-full h-full"
+            >
+              {locations.map((location) => {
+                return (
+                  <AdvancedMarker
+                    key={location.key}
+                    position={location.location}
+                    onClick={() => alert(`This is ${location.key}`)}
+                  >
+                    <img src="/home_map_icon.png" width={32} height={32} />
+                  </AdvancedMarker>
+                );
+              })}
+            </Map>
+          ) : (
+            <Map
+              defaultCenter={myLocation}
+              defaultZoom={15}
+              mapId={mapId}
+              mapTypeId="roadmap"
+              streetViewControl={false}
+              mapTypeControl={false}
+              gestureHandling={'greedy'}
+              zoomControl={false}
+              className="w-full h-full"
+            >
+              {locations.map((location) => {
+                return (
+                  <AdvancedMarker
+                    key={location.key}
+                    position={location.location}
+                    onClick={() => alert(`This is ${location.key}`)}
+                  >
+                    <img src="/home_map_icon.png" width={32} height={32} />
+                  </AdvancedMarker>
+                );
+              })}
+            </Map>
+          )}
         </APIProvider>
       ) : (
         ''
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,19 @@ import Filter from '../components/Filter';
 import Buttons from '../components/Buttons';
 import HouseCarousel from '../components/HouseCarousel';
 import MapComp from '../components/MapComp';
+import { useState } from 'react';
 export default function Home() {
+  const [houseCoords, setHouseCoords] = useState(null);
   return (
     <>
       <div>
         <Filter />
         <Buttons />
-        <HouseCarousel />
-        <MapComp />
+        <HouseCarousel
+          houseCoords={houseCoords}
+          setHouseCoords={setHouseCoords}
+        />
+        <MapComp houseCoords={houseCoords} setHouseCoords={setHouseCoords} />
       </div>
     </>
   );
