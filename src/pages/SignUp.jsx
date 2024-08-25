de7fc987a5595697081e0ff6ef96f8effee90e32
Arion Samarxhiu <arionsamarxhiu32@gmail.com>
2024-08-25 15:14:24 +0200

    SignUp process completed

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,12 +1,31 @@
 import { useForm } from 'react-hook-form';
+import { signUp } from '../controllers/userApi';
+import { useState } from 'react';
 
 export default function SignUp() {
   const { register, handleSubmit } = useForm();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
+  const onSubmit = async () => {
+    if (password === confirmPassword) {
+      try {
+        await signUp({ firstName, lastName, email, password });
+        alert('Registered successfully!');
+      } catch (error) {
+        alert('Error registering: ' + error.message);
+      }
+    } else {
+      alert('Passwords do not match!');
+    }
+  };
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         className="bg-white p-8 rounded-lg shadow-md w-full max-w-md mb-[165px]"
       >
         <h2 className="text-2xl font-bold mb-6 text-center">
@@ -19,6 +38,7 @@ export default function SignUp() {
             {...register('firstName', { required: true })}
             placeholder="First name"
             className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            onChange={(e) => setFirstName(e.target.value)}
           />
         </div>
 
@@ -28,6 +48,7 @@ export default function SignUp() {
             {...register('lastName', { required: true })}
             placeholder="Last name"
             className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            onChange={(e) => setLastName(e.target.value)}
           />
         </div>
 
@@ -37,6 +58,7 @@ export default function SignUp() {
             {...register('email', { required: true })}
             placeholder="Email"
             className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -46,22 +68,21 @@ export default function SignUp() {
             {...register('password', { required: true })}
             placeholder="Password"
             className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
         <div className="mb-6">
           <input
             type="password"
-            {...register('password', { required: true })}
+            {...register('passwordConfirm', { required: true })}
             placeholder="Confirm Password"
             className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </div>
 
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white p-3 rounded-md hover:bg-blue-600 transition-colors"
-        >
+        <button className="w-full bg-indigo-600 text-white p-3 rounded-md hover:bg-blue-600 transition-colors">
           Sign Up
         </button>
       </form>
